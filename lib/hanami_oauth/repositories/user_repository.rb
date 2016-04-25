53cbd6d7304ce755642b544e13441af4273da548
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2016-04-25 19:17:20 +0200

    [step 5] Add auth! to UserRepository

--- lib/hanami_oauth/repositories/user_repository.rb
+++ lib/hanami_oauth/repositories/user_repository.rb
@@ -1,3 +1,19 @@
 class UserRepository
   include Hanami::Repository
+
+  def self.auth!(auth_hash)
+    info = auth_hash[:info]
+    github_id = info[:uid].to_i
+    attrs = {
+      name:   info[:name],
+      email:  info[:email]
+    }
+
+    if user = query { where(github_id: attrs[:github_id]) }.first
+      user.update(attrs)
+      update user
+    else
+      create(User.new(attrs.merge(github_id: github_id)))
+    end
+  end
 end
--- spec/hanami_oauth/repositories/user_repository_spec.rb
+++ spec/hanami_oauth/repositories/user_repository_spec.rb
@@ -1,3 +1,50 @@
 RSpec.describe UserRepository do
-  # place your tests here
+  before do
+    UserRepository.clear
+  end
+
+  let(:user) { described_class.auth!(params) }
+  let(:params) do
+    {
+      info: {
+        uid: '123456',
+        name: 'Test',
+        email: 'test@example.com'
+      }
+    }
+  end
+
+  describe '#auth!' do
+    context 'when user exists' do
+      before do
+        described_class.auth!(params)
+      end
+
+      it 'has proper name' do
+        expect(user.name).to eq('Test')
+      end
+
+      it 'has proper email' do
+        expect(user.email).to eq('test@example.com')
+      end
+
+      it 'create user' do
+        expect { user }.to change { described_class.all.count }.by(1)
+      end
+    end
+
+    context 'when user no exists' do
+      it 'has proper name' do
+        expect(user.name).to eq('Test')
+      end
+
+      it 'has proper email' do
+        expect(user.email).to eq('test@example.com')
+      end
+
+      it 'create user' do
+        expect { user }.to change { described_class.all.count }.by(1)
+      end
+    end
+  end
 end
